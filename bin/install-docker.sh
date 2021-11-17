e6ee52b8de953aeb74fc0fc3d06222b95caedac7
Kyle Pericak <kyle@pericak.com>
2021-11-17 13:44:23 -0500

    docker script fix

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -5,3 +5,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
+# For some reason Docker doesn't really work right until you actually tell it to do something
+docker ps -a
