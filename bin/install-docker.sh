8af92013842cbd9cc89c74f9864cf84e971da4c0
Kyle Pericak <kyle@pericak.com>
2021-11-17 13:58:01 -0500

    updated docker cgroupfs to systemd

--- bin/install-docker.sh
+++ bin/install-docker.sh
@@ -5,5 +5,19 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
-# For some reason Docker doesn't really work right until you actually tell it to do something
-docker ps -a
+
+# kubeadm does not like cgroupfs on ubuntu 20's docker
+# See https://github.com/kubernetes/kubeadm/issues/1394
+if [[ "$(docker info 2>/dev/null | grep cgroupfs)" != "" ]]; then
+  cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+  systemctl restart docker
+fi
